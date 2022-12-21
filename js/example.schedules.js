6207f3a5b4fdae1c6eab2afb6a55ca8e6eec4ebf
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2022-12-21 13:39:13 -0600

    Remove unneeded comments

--- js/example.schedules.js
+++ js/example.schedules.js
@@ -6,29 +6,17 @@ const [year, month, date, day] = [
   time.getDay(),
 ];
 
-/*
-  ||
-  ||
-  ||
-  --
-  IMPORTANT - PLEASE READ
-  To configure look through the two demo schedules below and follow their comments
-  WHEN YOUR DONE
-  Scroll to the bottom and follow the instructions there to define the schedules so they can be picked up by Basil
-*/
-
 const demoEventSchedule = {
-  name: "Demo Event Schedule", // Used in case of fallback to this schedule
-  type: "event", // Either "event" or "weekRepeat". Read README for more info about each type.
-  dateValid: new Date("December 19, 2022"), // EVENT TYPE ONLY - Date valid for the event schedule
+  name: "Demo Event Schedule",
+  type: "event",
+  dateValid: new Date("December 19, 2022"),
   schedule: [
     {
-      name: "Item 1", // Label used in case of fallback
-      startTime: new Date(year, month, date, 7, 10), // The second to last number is the hour followed by the minute (24hr format)
-      endTime: new Date(year, month, date, 8, 10), // The second to last number is the hour followed by the minute (24hr format)
-      url: "https://www.google.com", // The URL opened for this item. It can also be null (no commas) to fallback to show the list. Useful for break periods.
+      name: "Item 1",
+      startTime: new Date(year, month, date, 7, 10),
+      endTime: new Date(year, month, date, 8, 10),
+      url: "https://www.google.com",
     },
-    // THIS ITEM BELOW IS JUST TO SHOW HOW TO ADD MORE ITEMS. FEEL FREE TO DELETE THIS COMMENT
     {
       name: "Item 2",
       startTime: new Date(year, month, date, 8, 20),
@@ -39,17 +27,16 @@ const demoEventSchedule = {
 };
 
 const demoNormalSchedule = {
-  name: "Normal Schedule", // Used in case of fallback to this schedule
-  type: "weekRepeat", // Either "event" or "weekRepeat". Read README for more info about each type.
-  daysValid: [1, 2, 3, 4], // WEEK REPEAT TYPE ONLY - A comma seperated list representing days of the week. 0 = Sunday, 1 = Monday and so on.
+  name: "Normal Schedule",
+  type: "weekRepeat",
+  daysValid: [1, 2, 3, 4],
   schedule: [
     {
-      name: "Item 1", // Label used in case of fallback
-      startTime: new Date(year, month, date, 7, 10), // The second to last number is the hour followed by the minute (24hr format)
-      endTime: new Date(year, month, date, 8, 10), // The second to last number is the hour followed by the minute (24hr format)
-      url: "https://www.google.com", // The URL opened for this item. It can also be null (no commas) to fallback to show the list. Useful for break periods.
+      name: "Item 1",
+      startTime: new Date(year, month, date, 7, 10),
+      endTime: new Date(year, month, date, 8, 10),
+      url: "https://www.google.com",
     },
-    // THIS ITEM BELOW IS JUST TO SHOW HOW TO ADD MORE ITEMS. FEEL FREE TO DELETE THIS COMMENT
     {
       name: "Item 2",
       startTime: new Date(year, month, date, 8, 20),
@@ -58,13 +45,6 @@ const demoNormalSchedule = {
     },
   ],
 };
-
-/* 
-    Instructions to Define Schedule:
-    In schedules variable, add all the schedules defined above. Otherwise they will be ignored.
-    In the fallbackSchedule variable, add a schedule to display the links to if no available schedule is available
-    Alternatively, set the variable to null to just load a placeholder
-*/
 
 const schedules = [demoEventSchedule, demoNormalSchedule];
 const fallbackSchedule = demoNormalSchedule;
