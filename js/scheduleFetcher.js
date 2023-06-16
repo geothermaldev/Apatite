81d577cd420907c934da602475611eac0d5452dc
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2023-06-15 21:03:25 -0500

    Remove setting a property to check for success

--- js/scheduleFetcher.js
+++ js/scheduleFetcher.js
@@ -40,10 +40,6 @@ const eventScheduleHandler = () => {
 
   // Here we validate to see if there if more or less than one event schedule for today and act accordingly
   if (eventSchedule.length > 1) {
-    // Set the schedule var to mark a fallback occured
-    schedule = {
-      fallbackOccured: true,
-    };
     // Call the fallback handler to handle the problem
     fallbackHandler(
       "Multiple event schedules have been specified for this date. Verify that you don't have more than one event schedule on the same day."
@@ -56,7 +52,7 @@ const eventScheduleHandler = () => {
     if (weekRepeatSchedules.length > 0) {
       weekRepeatScheduleHandler();
     } else {
-      fallbackHandler(null);
+      fallbackHandler();
     }
   } else {
     // Otherwise set the schedule variable to the first (and only) item in the eventSchedule array
@@ -77,16 +73,13 @@ const weekRepeatScheduleHandler = () => {
 
   // Here we validate to see if there if more than one event schedule for today and act accordingly
   if (weekRepeatSchedule.length > 1) {
-    // Set the schedule var to mark a fallback occured
-    schedule = {
-      fallbackOccured: true,
-    };
+    // // Set the schedule var to mark a fallback occured
     fallbackHandler(
       "Multiple repeating schedules have been specified for this date. Verify that you don't have more than one repeating schedule on the same day."
     );
     // Call the fallback handler to handle the problem
   } else if (weekRepeatSchedule.length < 1) {
-    fallbackHandler(null);
+    fallbackHandler();
   } else {
     // Otherwise set the schedule variable to the first (and only) item in the eventSchedule array
     schedule = weekRepeatSchedule[0];
@@ -102,9 +95,6 @@ if (eventSchedules.length > 0) {
 } else if (weekRepeatSchedules.length > 0) {
   weekRepeatScheduleHandler();
 } else {
-  schedule = {
-    fallbackOccured: true,
-  };
   fallbackHandler(
     "No schedules have been found. Make sure your schedules are defined correctly in the schedules.js file."
   );
