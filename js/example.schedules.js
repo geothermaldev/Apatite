aeea339de6bb8939345bd7544fb39274b68dd3ab
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2023-08-19 22:01:50 -0500

    Update example schedules file

--- js/example.schedules.js
+++ js/example.schedules.js
@@ -1,59 +1,20 @@
-// Create a class to reduce potential user errors when creating a new schedule
-class ScheduleTime {
-  constructor(hour, minutes) {
-    const time = new Date();
-    const [year, month, date, day] = [
-      time.getFullYear(),
-      time.getMonth(),
-      time.getDate(),
-      time.getDay(),
-    ];
+const demoEventSchedule = new EventSchedule(
+  "Demo Event Schedule",
+  new Date("December 19, 2022"),
+  [
+    new ScheduleItem("Item 1", [7, 10], [8, 10], "https://example.com"),
+    new ScheduleItem("Item 2", [8, 20], [9, 20], "https://example.com"),
+  ]
+);
 
-    return new Date(year, month, date, hour, minutes);
-  }
-}
-
-// INSERT SCHEDULES HERE
-
-const demoEventSchedule = {
-  name: "Demo Event Schedule",
-  type: "event",
-  dateValid: new Date("December 19, 2022"),
-  schedule: [
-    {
-      name: "Item 1",
-      startTime: new ScheduleTime(7, 10),
-      endTime: new ScheduleTime(8, 10),
-      url: "https://www.google.com",
-    },
-    {
-      name: "Item 2",
-      startTime: new ScheduleTime(8, 20),
-      endTime: new ScheduleTime(9, 20),
-      url: "https://www.google.com",
-    },
-  ],
-};
-
-const demoNormalSchedule = {
-  name: "Normal Schedule",
-  type: "weekRepeat",
-  daysValid: [1, 2, 3, 4, 5],
-  schedule: [
-    {
-      name: "Item 1",
-      startTime: new ScheduleTime(11, 10),
-      endTime: new ScheduleTime(12, 10),
-      url: "https://www.google.com",
-    },
-    {
-      name: "Item 2",
-      startTime: new ScheduleTime(12, 20),
-      endTime: new ScheduleTime(13, 20),
-      url: "https://www.google.com",
-    },
-  ],
-};
+const demoNormalSchedule = new WeekRepeatSchedule(
+  "Normal Schedule",
+  [1, 2, 3, 4, 5],
+  [
+    new ScheduleItem("Item 1", [11, 10], [12, 10], "https://example.com"),
+    new ScheduleItem("Item 2", [12, 20], [13, 20], "https://example.com"),
+  ]
+);
 
 const schedules = [demoEventSchedule, demoNormalSchedule];
 const fallbackSchedule = demoNormalSchedule;
